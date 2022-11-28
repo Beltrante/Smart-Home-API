d4d41c66107987ccbf9cc84158bbeccaf986a4e5
MarcoBendinelli <marco.bendinelli@mail.polimi.it>
2022-11-28 23:12:45 +0100

    First setting

--- index.js
+++ index.js
@@ -12,25 +12,7 @@ app.get('/', (req, res) => {
         .end();
 });
 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: 'TSHIRT',
-        size: 'large'
-    })
-});
 
-app.post('/tshirt/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
-
-    if (!logo) {
-        res.status(418).send({ message: 'We need a logo! '});
-    }
-
-    res.send({
-        tshirt: `TSHIRT with your ${logo} and ID of ${id}`
-    });
-});
 
 app.listen(
     PORT,
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res
         .status(200)
-        .send('Hello server is running')
+        .send('Smart Home API Server is running')
         .end();
 });
 
@@ -13,4 +13,24 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
     console.log('Press Ctrl+C to quit.');
+});
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'TSHIRT',
+        size: 'large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo! '});
+    }
+
+    res.send({
+        tshirt: `TSHIRT with your ${logo} and ID of ${id}`
+    });
 });
