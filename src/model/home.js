8bd7d43f7de2fe270f19ab255299766659e7578a
Lorenzo Poretti <lorylap98@gmail.com>
2022-12-22 13:21:12 +0100

    Add getAppliance by name

--- src/model/home.js
+++ src/model/home.js
@@ -5,7 +5,20 @@ import { WashingMachine } from '../model/appliance.js';
 class Home {
     constructor(){
         this.battery = new Battery(10,10,1)
-        this.appliances = [new DishWasher("dishwasher"), new WashingMachine("washingmachine")]
+        this.appliances = [
+            new DishWasher("dishwasher"), 
+            new WashingMachine("washingmachine")]
+    }
+
+    // Return the appliance with the rispective name passed as parameter.
+    // Return null if no appliace is found
+    getAppliance(name){
+        for(let i = 0; i < this.appliances.length; i++){
+            if(name == this.appliances[i].name){
+                return this.appliances[i];
+            }
+        }
+        return null;
     }
 }
 
--- src/routers/applianceRouter.js
+++ src/routers/applianceRouter.js
@@ -3,6 +3,26 @@ import home from '../model/home.js';
 
 const router = new express.Router();
 
+router.get('/appliances/:name', (req, res) => {
+    const name = req.params.name;
+    const appliance = home.getAppliance(name)
+    if(appliance == null){
+        res.status(404)
+        .send({
+            message: 'No appliance with name = ' + name
+        })
+    }
+    else{
+        res
+        .status(200)
+        .send({
+            name: home.appliances[0].name,
+            unitOfMeasure: 'kWh'
+        })
+        .end();
+    }
+});
+
 router.get('/appliances', (req, res) => {
 
     res
